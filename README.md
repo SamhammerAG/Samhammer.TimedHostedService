637e5f3e19194f6c6e0a7d9dc84c126b2f344666
Stefan Rom <srom@samhammer.de>
2020-04-22 13:49:16 +0200

    KIT-82 Add new project with a timedHostedService

---
+++ README.md
@@ -0,0 +1,66 @@
+﻿[![Build Status](https://travis-ci.com/SamhammerAG/Samhammer.TimedHostedService.svg?branch=master)](https://travis-ci.com/SamhammerAG/Samhammer.TimedHostedService)
+
+## Samhammer.TimedHostedService
+
+This package provides a hosted service that can run periodically.
+
+The concept is from this documentation:
+https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-3.1&tabs=visual-studio
+
+#### How to add this to your project:
+- reference this nuget package: https://www.nuget.org/packages/Samhammer.TimedHostedService/
+
+#### How to use:
+
+Implement a class that inherits TimedHostedService.
+```csharp
+public class MyHostedService : TimedHostedService<IMyService>
+{
+	protected override TimeSpan StartDelay => TimeSpan.FromSeconds(3);
+
+	protected override TimeSpan ExecutionInterval => TimeSpan.FromDays(1);
+	
+	public MyHostedService(ILogger<MyHostedService> logger, IServiceScopeFactory services) 
+		: base(logger, services)
+	{
+	}
+	
+	protected override Task RunScoped(IMyService myService)
+	{
+		myService.DoSomething();
+		return Task.CompletedTask;
+	}
+}
+```
+
+Register the hosted service in startup:
+```csharp
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddHostedService<MyHostedService>()
+}
+```
+
+Possible configurations:
+- ExecutionInterval: How often the job ticks after starting (default: 60 seconds)
+- StartDelay: Delay the first tick for a specific time after starting (default: 0)
+- OnlySingleInstance: If the last tick is still no other tick is started even if the executionInterval is over (default: false)
+
+Possible hook points:
+- RunScoped: Execute your logic here (mandatory)
+- OnStartup: Is called before the timer starts ticking (optional)
+- OnRunSuccessful: Is executed after the current tick is over (optional)
+- OnError:  Can be used to handle errors that occur within a running tick
+
+Note:
+- Errors inside "ExecutionInterval" are catched and can be handled with "OnError"
+- "RunScoped" is running in it's ioc scope. The other hook points are outside of this scope.
+
+## Contribute
+
+#### How to publish a nuget package
+- set package version in Samhammer.TimedHostedService.csproj
+- create git tag
+- dotnet pack -c Release
+- nuget push .Samhammer.TimedHostedService\bin\Release\Samhammer.TimedHostedService.*.nupkg NUGET_API_KEY -src https://api.nuget.org/v3/index.json
+- (optional) nuget setapikey NUGET_API_KEY -source https://api.nuget.org/v3/index.json
---
+++ src/Samhammer.TimedHostedService/TimedHostedService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Samhammer.TimedHostedService
+{
+    public abstract class TimedHostedService<T> : IHostedService, IDisposable
+    {
+        protected virtual TimeSpan StartDelay => TimeSpan.MinValue;
+
+        protected virtual TimeSpan ExecutionInterval => TimeSpan.FromSeconds(60);
+
+        protected virtual bool OnlySingleInstance => false;
+
+        protected ILogger<TimedHostedService<T>> Logger { get; }
+
+        protected IServiceScopeFactory Services { get; }
+
+        private bool IsRunning { get; set; }
+
+        private Timer Timer { get; set; }
+
+        protected TimedHostedService(ILogger<TimedHostedService<T>> logger, IServiceScopeFactory services)
+        {
+            Logger = logger;
+            Services = services;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await OnStartup();
+
+            Logger.LogInformation("TimedHostedService {HostedService} is starting. (Tick interval: {Interval})", typeof(T), ExecutionInterval);
+
+            Timer = new Timer(TimerTickCallback, null, StartDelay, ExecutionInterval);
+        }
+
+        protected virtual Task OnStartup()
+        {
+            return Task.CompletedTask;
+        }
+
+        private async void TimerTickCallback(object state)
+        {
+            try
+            {
+                Logger.LogDebug("TimedHostedService {HostedService} tick starting.", typeof(T));
+                if (OnlySingleInstance && IsRunning)
+                {
+                    Logger.LogDebug("TimedHostedService {HostedService} tick stopping, because one instance is already running.", typeof(T));
+                    return;
+                }
+
+                IsRunning = true;
+                await Run();
+                await OnRunSuccessful();
+                IsRunning = false;
+
+                Logger.LogDebug("TimedHostedService {HostedService} tick finished.", typeof(T));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "TimedHostedService {HostedService} tick failed with an error, aborting current tick.", typeof(T));
+                await OnError();
+                IsRunning = false;
+            }
+        }
+
+        protected virtual Task OnRunSuccessful()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task OnError()
+        {
+            return Task.CompletedTask;
+        }
+
+        private async Task Run()
+        {
+            using (var scope = Services.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetRequiredService<T>();
+                await RunScoped(service);
+            }
+        }
+
+        protected abstract Task RunScoped(T service);
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await OnStopping();
+
+            Logger.LogInformation("TimedHostedService {HostedService} is stopping.", typeof(T));
+
+            Timer?.Change(Timeout.Infinite, 0);
+        }
+
+        protected virtual Task OnStopping()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Timer?.Dispose();
+        }
+    }
+}
